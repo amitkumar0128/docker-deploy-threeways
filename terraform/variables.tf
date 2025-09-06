ce55e6efe32097a457baff95f7ebb6ed46090553
Jangid Amitkumar <134952776+amitkumar0128@users.noreply.github.com>
2025-09-06 21:55:05 +0530

    fix: change repo link and name in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,13 +2,13 @@
 variable "github_repo_url" {
   description = "Public GitHub repo to clone containing a Jenkinsfile"
   type        = string
-  default     = "https://github.com/amitkumar0128/terra-ansible-jenkins-docker.git"
+  default     = "https://github.com/amitkumar0128/docker-deploy-threeways.git"
 }
 
 variable "github_repo_name" {
   description = "Public GitHub repo name"
   type        = string
-  default     = "terra-ansible-jenkins-docker"
+  default     = "docker-deploy-threeways"
 }
 
 variable "instance_type" {
